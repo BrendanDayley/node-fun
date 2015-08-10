3b5fe46da186ccdb8cbfa96157c76c2e799ce525
BrendanDayley <brendandayley@gmail.com>
2015-08-10 13:59:29 -0600

    ready to send messages

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 /* global process */
 
 'use strict';
+
+//#############################################################
+//		 Twilio Credentials
+//#############################################################
+var accountSid = '';
+var authToken = '';
+
 //#############################################################
 //		 DEPENDANCIES
 //#############################################################
@@ -8,9 +15,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var request = require('request');
+var twilio = require('twilio')
 
 var app = express();
 var port =  process.argv[2] || 8181;
+var client = twilio(accountSid, authToken);
+
+
+
 
 //#############################################################
 //		 MIDDLEWARE
@@ -35,8 +47,31 @@ app.get('/api/message', function(req, res){
 
 app.post('/api/send_text_message', function (req, res){
 	console.log(req.body.message);
-	res.json();
+	
+	client.messages.create({
+	//to: "2089641796",
+	to: "8013617836",
+	from: "+18014712323",
+	mediaUrl: req.body.message,   
+}, function(err, message) { 
+	if(err){
+		console.log('aahhhh!!: ', err);
+	}
+	console.log(message.sid); 
+})
+
+	res.json(req.body.message);
 });
+
+app.post('/api/send_text_message', function(req, res){
+	console.log(req.body.message);
+	req.post('https://' + accountSid + ':' + authToken + '@api.twilio.com/2010-04-01/Accounts/')
+})
+
+
+
+
+
 app.listen(port, function(){
 	console.log('listening on port: ' + port);
 });
