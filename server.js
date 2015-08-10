b2a7ee6354ac464657a09b16f98431fac9791b22
BrendanDayley <brendandayley@gmail.com>
2015-08-10 12:48:48 -0600

    final commit of the server for this example

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+/* global __dirname */
 /* global process */
+
 'use strict';
 //#############################################################
 //		 DEPENDANCIES
@@ -7,15 +9,34 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-
-//#############################################################
-//		 DEPENDANCY INVOCATION
-//#############################################################
 var app = express();
-
 var port =  process.argv[2] || 8181;
 
+//#############################################################
+//		 MIDDLEWARE
+//#############################################################
+app.use(express.static(__dirname + 'public'));
+app.use('/', bodyParser.json());
+app.use('/', cors());
 
+//#############################################################
+//		 Variables
+//#############################################################
+var message = {
+	"message": "hello World!!!!!"
+}
+
+//#############################################################
+//		 ENDPOINTS
+//#############################################################
+app.get('/api/message', function(req, res){
+	res.json({"message": "HELLO WORLD!!!!!"})
+});
+
+app.post('/api/receive_message', function (req, res){
+	console.log(req.body);
+	res.json(req.body);
+});
 app.listen(port, function(){
 	console.log('listening on port: ' + port);
 });
